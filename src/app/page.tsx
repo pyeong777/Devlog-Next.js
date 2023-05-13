dba78b281ef04563b16701e6463ae630cb3ed3e6
pyeong777 <sdp1123@naver.com>
2023-05-13 14:34:18 +0900

    Move: 컴포넌트 분리

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,11 @@
-import Image from "next/image";
+import FeaturedPosts from "@/components/FeaturedPosts";
+import Introduction from "@/components/Introduction";
 
-export default function Home() {
+export default function HomePage() {
   return (
-    <div className="flex items-center justify-between">
-      <article className="py-12 basis-4/12">
-        <h2>
-          프론트엔드 개발자의 기술 블로그 경험한 것들을 기록으로 남기고
-          있습니다.
-        </h2>
-        <h3 className="mt-3 text-sm text-gray-400">
-          {"A front-end developer's technical blog to record experiences."}
-        </h3>
-      </article>
-      <article className="basis-8/12">
-        <Image
-          className="m-auto animate-bounce"
-          priority
-          src="/assets/img.svg"
-          width={100}
-          height={100}
-          alt="logo"
-        />
-      </article>
-    </div>
+    <article>
+      <Introduction />
+      <FeaturedPosts />
+    </article>
   );
 }
