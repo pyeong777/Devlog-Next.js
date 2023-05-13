da7d64a7f7288b2a7ed27e301650e7ad9c044c46
pyeong777 <sdp1123@naver.com>
2023-05-13 14:34:54 +0900

    feat: Introduction 컴포넌트 추가

---
+++ src/components/Introduction.tsx
@@ -0,0 +1,27 @@
+import Image from "next/image";
+
+export default function Introduction() {
+  return (
+    <section className="flex items-center justify-between">
+      <div className="py-12 basis-4/12">
+        <h2>
+          프론트엔드 개발자의 기술 블로그 경험한 것들을 기록으로 남기고
+          있습니다.
+        </h2>
+        <h3 className="mt-3 text-sm text-gray-400">
+          {"A front-end developer's technical blog to record experiences."}
+        </h3>
+      </div>
+      <div className="basis-8/12">
+        <Image
+          className="m-auto animate-bounce"
+          priority
+          src="/assets/img.svg"
+          width={100}
+          height={100}
+          alt="Introduction svg"
+        />
+      </div>
+    </section>
+  );
+}
