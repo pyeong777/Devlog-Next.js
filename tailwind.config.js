ca9320ec411b70ea3b91c98b9788e1ce99d2f65a
pyeong777 <sdp1123@naver.com>
2023-05-14 23:18:31 +0900

    테마컬러 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,12 @@ module.exports = {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  theme: {},
+  theme: {
+    extend: {
+      colors: {
+        "my-color": "#12B886",
+      },
+    },
+  },
   plugins: [],
 };
