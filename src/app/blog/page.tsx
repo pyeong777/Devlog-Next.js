9230be292f02a3dda1cdc2a0aa44a1b4eb24ffeb
pyeong777 <sdp1123@naver.com>
2023-05-14 23:19:49 +0900

    feat: 블로그 페이지 추가

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,3 +1,12 @@
-export default function blogPage() {
-  return <>여기는 블로그 포스트</>;
+import FilterablePosts from "@/components/FilterablePosts";
+import { getAllPosts } from "@/service/posts";
+
+export default async function blogPage() {
+  const posts = await getAllPosts();
+  const categories = [...new Set(posts.map((post) => post.category))];
+  return (
+    <article className="flex flex-col my-16">
+      <FilterablePosts posts={posts} categories={categories} />
+    </article>
+  );
 }
