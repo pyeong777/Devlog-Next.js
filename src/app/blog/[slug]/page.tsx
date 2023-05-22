1863b9ec173e29709c6d311992167d3ee616e3fb
pyeong777 <sdp1123@naver.com>
2023-05-23 03:28:55 +0900

    feat: 포스트 페이지 & 마크다운 컴포넌트 추가

---
+++ src/app/blog/[slug]/page.tsx
@@ -0,0 +1,33 @@
+import Image from "next/image";
+import MarkDownViewer from "@/components/MarkdownViewer";
+import { getPostData } from "@/service/posts";
+
+type Props = {
+  params: {
+    slug: string;
+  };
+};
+
+export default async function PostPage({ params: { slug } }: Props) {
+  const { title, description, date, path, content } = await getPostData(slug);
+  return (
+    <article className="m-4">
+      <section className="flex flex-col p-4 m-auto">
+        <h1 className="mb-5 text-3xl font-bold">{title}</h1>
+        <div className="flex">
+          <p>{description}&nbsp;·&nbsp;</p>
+          <p className="text-gray-700">{date.toString()}</p>
+        </div>
+        <div className="mt-4 mb-8 border-2 w-44 border-my-color" />
+        <Image
+          className="mb-4"
+          src={`/images/posts/${path}.png`}
+          alt={title}
+          width={760}
+          height={420}
+        />
+        <MarkDownViewer content={content} />
+      </section>
+    </article>
+  );
+}
