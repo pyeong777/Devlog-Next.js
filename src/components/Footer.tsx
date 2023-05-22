5ac2d6fe4af672a45feea44f637a25406a870582
pyeong777 <sdp1123@naver.com>
2023-05-23 03:27:38 +0900

    design: footer css 추가

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,13 +1,18 @@
 import Link from "next/link";
+import { AiFillGithub } from "react-icons/ai";
 
 export default function Footer() {
   return (
-    <footer className="py-4 text-sm text-center border border-t-gray-300">
-      <p className="py-2">깃허브</p>
+    <footer className="flex flex-col items-center py-4 text-sm text-center border border-t-gray-300">
+      <p className="py-2">
+        <a href="https://github.com/pyeong777" target="_blank">
+          <AiFillGithub className="w-8 h-8" />
+        </a>
+      </p>
       <Link href="/" className="py-2">
         <p>Pyeong devlog</p>
       </Link>
-      <p className="py-2">daepyeongseo All rights reserved</p>
+      <address className="py-2">&copy;daepyeongseo All rights reserved</address>
     </footer>
   );
 }
