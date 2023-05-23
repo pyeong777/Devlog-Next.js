db5b795492c95a7312c16f74c8054b708becda01
pyeong777 <sdp1123@naver.com>
2023-05-24 00:26:20 +0900

    feat: contact 페이지 추가

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,3 +1,23 @@
+import ContactForm from "@/components/ContactForm";
+import { AiFillGithub } from "react-icons/ai";
+
 export default function contactPage() {
-  return <>여기는 연락 페이지</>;
+  return (
+    <section className="flex flex-col items-center">
+      <h2 className="my-2 text-2xl font-bold">Contact Me</h2>
+      <p>seowns9520@gmail.com</p>
+      <div className="flex gap-4 my-2">
+        <a
+          href="https://github.com/pyeong777"
+          target="_blank"
+          rel="noreferrer"
+          className="text-4xl hover:text-my-color"
+        >
+          <AiFillGithub />
+        </a>
+      </div>
+      <h2 className="my-8 text-2xl font-bold">Send me an email</h2>
+      <ContactForm />
+    </section>
+  );
 }
