25fece07da68e04ac93d8cf7aea0522189197312
pyeong777 <seowns9520@gmail.com>
2023-06-01 23:14:18 +0900

    feat: 모든 포스트 Link태그 추가

--- src/components/FeaturedPosts.tsx
+++ src/components/FeaturedPosts.tsx
@@ -1,5 +1,6 @@
 import { getFeaturedPosts } from "@/service/posts";
 import PostGrid from "./PostGrid";
+import Link from "next/link";
 
 export default async function FeaturedPosts() {
   const posts = await getFeaturedPosts();
@@ -7,6 +8,11 @@ export default async function FeaturedPosts() {
     <section className="my-16">
       <h2 className="text-2xl font-bold">Post</h2>
       <PostGrid posts={posts} />
+      <div className="flex justify-end">
+        <Link className="text-my-color" href="/blog">
+          모든 포스트 &rarr;
+        </Link>
+      </div>
     </section>
   );
 }
