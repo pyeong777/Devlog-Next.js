5f3fbe935ca1bb0d0a04535ba79725dc7f00a756
pyeong777 <seowns9520@gmail.com>
2023-06-01 23:13:50 +0900

    remove: 캐러셀 삭제

--- src/components/CarouselPosts.tsx
+++ src/components/CarouselPosts.tsx
@@ -1,17 +1,17 @@
-import { getNonFeaturedPosts } from "@/service/posts";
-import PostCard from "./PostCard";
-import MultiCarousel from "./MultiCarousel";
+// import { getNonFeaturedPosts } from "@/service/posts";
+// import PostCard from "./PostCard";
+// import MultiCarousel from "./MultiCarousel";
 
-export default async function CarouselPosts() {
-  const posts = await getNonFeaturedPosts();
-  return (
-    <section className="my-16">
-      <h2 className="pt-10 text-2xl font-bold">Recommended</h2>
-      <MultiCarousel>
-        {posts.map((post) => (
-          <PostCard key={post.path} post={post} />
-        ))}
-      </MultiCarousel>
-    </section>
-  );
-}
+// export default async function CarouselPosts() {
+//   const posts = await getNonFeaturedPosts();
+//   return (
+//     <section className="my-16">
+//       <h2 className="pt-10 text-2xl font-bold">Recommended</h2>
+//       <MultiCarousel>
+//         {posts.map((post) => (
+//           <PostCard key={post.path} post={post} />
+//         ))}
+//       </MultiCarousel>
+//     </section>
+//   );
+// }
